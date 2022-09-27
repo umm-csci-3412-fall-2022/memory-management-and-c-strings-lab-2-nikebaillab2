16487f851467787117429658ca6013b4a5c37e98
Nik Bailey <bail0301@morris.umn.edu>
2022-09-27 16:23:50 -0500

    Added disemvowel function and freed pointers in disemvowel.c and main.c

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,24 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  char *consonants = calloc(strlen(str)+1, sizeof(char));
+  char *vowels = calloc(11, sizeof(char));
+  strcpy(vowels, "aeiouAEIOU\0");
+  int j = 0;
+  for(int i = 0; i < strlen(str); ++i) {
+	if(strchr(vowels, str[i]) == NULL) {
+		consonants[j++] = str[i];
+	}
+  }
+  consonants[j] = '\0';
+
+  char *result = calloc(strlen(consonants)+1, sizeof(char));
+  strcpy(result, consonants);
+  result[j] ='\0';
+
+  free((char*) consonants);
+  free((char*) vowels);
+  return result;
 }
--- disemvowel/main.c
+++ disemvowel/main.c
@@ -9,8 +9,12 @@ int main(int argc, char *argv[]) {
   
   size = 100;
   line = (char*) malloc (size + 1);
-
+  
   while (getline(&line, &size, stdin) > 0) {
-    printf("%s\n", disemvowel(line));
+    char* no_vowel = disemvowel(line);
+    printf("%s\n", no_vowel);
+    free(no_vowel);
   }
+
+  free((char*)line);
 }
